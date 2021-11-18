747affa7c2499ad7e9dbbfe7ff4b744cfd45315a
EMZIANE Rachid <91244734+rachid691@users.noreply.github.com>
2021-11-18 09:09:47 +0100

    Update calendrier.py

--- calendrier.py
+++ calendrier.py
@@ -12,3 +12,31 @@ def bis(annee):
     else:
         return True
 
+def nombrejour(annee,mois):
+    M31=[1,3,5,7,8,10,12]
+    if mois==2:
+        if bis(annee)==True:
+            return 29
+        else:
+            return 28
+    for i in M31:
+        return 31
+    return 30
+
+def datevalide(jour,mois,annee):
+    njour=nombrejour(annee,mois)
+    if  jour>njour:
+        return False
+    else:
+        return True
+
+jour=int(input("Numéro du jour?"))
+mois=int(input("Numéro du mois?"))
+while mois<=0 and mois>12:
+    mois=int(input("Numéro du mois entre 1 et 12?"))
+annee=int(input("Numéro de l'année?"))
+if datevalide(jour,mois,annee)==True:
+    print("date valide")
+else:
+    print("date non valide")
+
